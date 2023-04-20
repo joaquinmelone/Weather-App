0f8bf07089938b6f357acb097c4fa70ceb2d5a5c
Joaquin <joaquin.melone.zarauza@gmail.com>
2023-04-20 16:13:34 -0300

    Update src

--- src/index.js
+++ src/index.js
@@ -11,13 +11,3 @@ submitButton.addEventListener("click", e => {
 
     e.preventDefault();
 })
-
-//
-
-/* async function call() {
-    const a = await fetch("http://api.weatherapi.com/v1/current.json?key=16d000a993e1437ba2904850230404&q=London&aqi=no", {mode: 'cors'})
-
-    console.log(await a.json())
-}
-
-call(); */
